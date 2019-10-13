0a0c3c138de4f63513df244b2025232fdde10817
James Tang <jytang10@gmail.com>
2019-10-13 16:25:05 -0700

    Completed form-ui with name, email, password, password_confirmation text inputs

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet, Button} from 'react-native';
 import {Hoshi} from 'react-native-textinput-effects';
 
 class Register extends Component {
 
   state = {
-    name:''
+    name:'',
+    email:'',
+    password:'',
+    password_confirmation:''
   }
   render() {
     return (
       <View style={styles.container}>
-        <View style={{backgroundColor:'black', padding:10}}>
+        <View style={{backgroundColor:'black', padding:10, marginBottom:20}}>
           <Text style={{fontSize:25, color: 'white', alignSelf:'center'}}>Form Tutorial</Text>
         </View>
-        <Hoshi value={this.state.name} onChangeText={name => this.setState({name})}></Hoshi>
-        <Text>{this.state.name}</Text>
+        <Hoshi 
+          label={"name"}
+          backgroundColor={'#fff'}
+          borderColor={'#b76c94'}
+          borderHeight={3}
+          inputPadding={16}
+          value={this.state.name}
+          onChangeText={name => this.setState({name})}
+          >
+        </Hoshi>
+        <Hoshi 
+          label={"email"}
+          backgroundColor={'#fff'}
+          borderColor={'#b76c94'}
+          borderHeight={3}
+          inputPadding={16}
+          value={this.state.email}
+          onChangeText={email => this.setState({email})}
+          >
+        </Hoshi>
+        <Hoshi 
+          label={"password"}
+          secureTextEntry
+          backgroundColor={'#fff'}
+          borderColor={'#b76c94'}
+          borderHeight={3}
+          inputPadding={16}
+          value={this.state.password}
+          onChangeText={password => this.setState({password})}
+          >
+        </Hoshi>
+        <Hoshi 
+          label={"Reconfirm password"}
+          secureTextEntry
+          backgroundColor={'#fff'}
+          borderColor={'#b76c94'}
+          borderHeight={3}
+          inputPadding={16}
+          style={{margineBottom:45}}
+          value={this.state.password_confirmation}
+          onChangeText={password_confirmation => this.setState({password_confirmation})}
+          >
+        </Hoshi>
+        <Button title="Register" onPress={() => {}}> </Button>
       </View>
     );
   }
@@ -23,7 +68,7 @@ class Register extends Component {
 
 const styles = StyleSheet.create({
   container: {
-
+    padding: 10
   },
 });
 
