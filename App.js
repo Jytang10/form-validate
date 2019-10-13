a088fad8bbec5f41a12dacf5f227193e69541997
James Tang <jytang10@gmail.com>
2019-10-13 16:40:18 -0700

    Added registerUser func

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-//import liraries
 import React, { Component } from 'react';
+import {validateAll} from 'indicative/validator';
 import { View, Text, StyleSheet, Button} from 'react-native';
 import {Hoshi} from 'react-native-textinput-effects';
 
@@ -11,6 +11,21 @@ class Register extends Component {
     password:'',
     password_confirmation:''
   }
+
+  registerUser = async(data) => {
+    const rules = {
+      name:'required|string',
+      email:'required|email',
+      password:'required|string|min:6|confirmed',
+    }
+
+    try {
+      await validateAll(data, rules, message)
+    } catch {
+
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -18,6 +33,7 @@ class Register extends Component {
           <Text style={{fontSize:25, color: 'white', alignSelf:'center'}}>Form Tutorial</Text>
         </View>
         <Hoshi 
+          style={{marginBottom:20}}
           label={"name"}
           backgroundColor={'#fff'}
           borderColor={'#b76c94'}
@@ -27,7 +43,8 @@ class Register extends Component {
           onChangeText={name => this.setState({name})}
           >
         </Hoshi>
-        <Hoshi 
+        <Hoshi
+          style={{marginBottom:20}}
           label={"email"}
           backgroundColor={'#fff'}
           borderColor={'#b76c94'}
@@ -37,7 +54,8 @@ class Register extends Component {
           onChangeText={email => this.setState({email})}
           >
         </Hoshi>
-        <Hoshi 
+        <Hoshi
+          style={{marginBottom:20}}
           label={"password"}
           secureTextEntry
           backgroundColor={'#fff'}
@@ -55,12 +73,12 @@ class Register extends Component {
           borderColor={'#b76c94'}
           borderHeight={3}
           inputPadding={16}
-          style={{margineBottom:45}}
+          style={{marginBottom:45}}
           value={this.state.password_confirmation}
           onChangeText={password_confirmation => this.setState({password_confirmation})}
           >
         </Hoshi>
-        <Button title="Register" onPress={() => {}}> </Button>
+        <Button title="Register" onPress={() => this.registerUser(this.state)}> </Button>
       </View>
     );
   }
