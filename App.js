70ec2d452eb4a3853c4ae91276705e3d930aa413
James Tang <jytang10@gmail.com>
2019-10-13 16:13:39 -0700

    Added Hoshi text input

--- App.js
+++ App.js
@@ -1,27 +1,30 @@
 //import liraries
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet} from 'react-native';
+import {Hoshi} from 'react-native-textinput-effects';
 
-// create a component
 class Register extends Component {
+
+  state = {
+    name:''
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Register</Text>
+        <View style={{backgroundColor:'black', padding:10}}>
+          <Text style={{fontSize:25, color: 'white', alignSelf:'center'}}>Form Tutorial</Text>
+        </View>
+        <Hoshi value={this.state.name} onChangeText={name => this.setState({name})}></Hoshi>
+        <Text>{this.state.name}</Text>
       </View>
     );
   }
 }
 
-// define your styles
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
+
   },
 });
 
-//make this component available to the app
 export default Register;
